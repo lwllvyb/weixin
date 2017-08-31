fb1d722c2e0cb559016e93cd9aac66db2eab69d6
liwenlong03 <liwenlong03@sankuai.com>
2017-08-31 17:54:33 +0800

    add except handle to reload config

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 #-*-coding:utf-8 -*-
 import configparser
+import signal
+
 PROJECTS_CONFIG = "projects.ini"
 
 
 class Parser():
     def __init__(self):
         pass
-    @staticmethod 
+
+    @staticmethod
     def load():
 
         config = configparser.ConfigParser()
@@ -15,20 +18,57 @@ class Parser():
             value = config.get(section, 'name')  # -> "Python is fun!"
             Projects.add_projects({section: value})
 
+
 class Projects():
 
     PROJECTS = {
         # "001": "维权",
     }
     project_id_next = 1
+
     def __init__(self):
         pass
+
     def get_project_id(self):
         ret = self.project_id_next
         self.project_id_next += 1
         return ret
+
     @classmethod
     def add_projects(cls, project):
         cls.PROJECTS.update(project)
 
-Parser.load()
+# 捕捉异常信号
+class ExceptSignalHandle():
+    SIGUSR1 = 30
+    signal_handlers = None
+    @classmethod
+    def collect_handlers(cls):
+        cls.signal_handlers = [
+            {
+                "signal": cls.SIGUSR1, 
+                "handler": cls.handle_reload_project
+            },
+            # {
+            #     "signal": SIGUSR1, 
+            #     "handler": handle_reload_project
+            # },
+        ]    
+    @classmethod
+    def handle_reload_project(cls, sig, frame):
+        Parser.load()
+        print("{0} {1}".format(sig, frame))
+        signal.signal(cls.SIGUSR1, cls.handle_reload_project)
+
+    @classmethod
+    def handle_signal(cls):
+        cls.collect_handlers()
+        for except_handle in cls.signal_handlers:
+            except_handle["handler"](except_handle["signal"], None)
+
+ExceptSignalHandle.handle_signal()
+
+if __name__ == "__main__":
+    import time
+    while True:
+        time.sleep()
--- process_txt.py
+++ process_txt.py
@@ -10,9 +10,8 @@ def txt_process(content):
     return "{0}".format(help())
 
 
-
 def help():
     ret = ''
     for id, project in Projects.PROJECTS.items():
-        ret += "{0}\t{1}\r\n".format(id, project)
+        ret += "{0}\tab{1}\r\n".format(id, project)
     return ret
